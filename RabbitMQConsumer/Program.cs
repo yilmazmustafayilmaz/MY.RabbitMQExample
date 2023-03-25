a5dac6550198d72d7d8b42ed6599587e05c538b2
YILMAZ <YILMAZ@DESKTOP-RR24A2S>
2023-03-25 13:51:03 +0300

    Add project files.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { Uri = new Uri("amqps://lumyzppt:AEBg0q_ZsuhKPOWccUJL4mPMyl4Ird8J@crow.rmq.cloudamqp.com/lumyzppt") };
+using var connection = factory.CreateConnection();
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Example", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, bdea) =>
+    {
+        var body = bdea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+        Product product = JsonConvert.DeserializeObject<Product>(message);
+        Console.WriteLine($"{product.Name} - {product.Description}");
+    };
+    channel.BasicConsume(queue: "Example", autoAck:true ,consumer: consumer);
+    Console.WriteLine("Mesaj teslim alındı..");
+    Console.ReadLine();
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+Product product = new Product() { Id = 1, Name = "Book", Description = "This is a Book" };
+
+var factory = new ConnectionFactory() { Uri = new Uri("amqps://lumyzppt:AEBg0q_ZsuhKPOWccUJL4mPMyl4Ird8J@crow.rmq.cloudamqp.com/lumyzppt") };
+using var connection = factory.CreateConnection();
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Example", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    string message = JsonConvert.SerializeObject(product);
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "", routingKey: "Example", basicProperties: null, body: body);
+
+    Console.WriteLine($"{product.Name}");
+}
+Console.WriteLine("Mesajınız teslim edildi..");
+Console.ReadLine();
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
